6538a87f00ff36286937223232c15aac4acc7f7c
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-03-20 22:43:48 -0300

    alteraçoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import Amazom from "./assets/amazom.png"
+import React, { useState, useEffect } from 'react'
+import Amazon from "./assets/amazon.png"
 import Sirio from "./assets/sirio.png"
 import Vnw from "./assets/vnw.png"
 import styled from "styled-components"
@@ -7,26 +7,27 @@ import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
   *{
-    margin:0; 
-    padding:0;
+    margin: 0; 
+    padding: 0;
     box-sizing: border-box;
   }
 `
 
-const Informacoes = styled.div`
-border: solid red;
+const Main = styled.section`
+`
+
+const Informacao = styled.section`
 display: flex;
 justify-content: space-around;
 align-items: center;
-height: 40vh;
+// height: 40vh;
 `
 
-const Patrocinadores = styled.div`
-  border: solid green;
+const Patrocinadores = styled.section`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-around;
   align-items: center;
-  height: 60vh;
+  // height: 60vh;
 `
 
 function App() {
@@ -34,40 +35,38 @@ function App() {
   const [marca, setMarca] = useState(Vnw)
 
   useEffect(() => {
+    
     if (marca === Vnw) {
-      document.querySelector("div").style.backgroundColor = "#F8BF15";
-      document.querySelector("div").style.transition="ease-in-out 0.8s"
-
-    } else if (marca === Amazon) {
-      document.querySelector("div").style.backgroundColor = "#FF00FF"
-      document.querySelector("div").style.transition="ease-in-out 0.8s"
-      document.querySelector("div > h2").style.color = "#ffffff"
-
-    } else if(marca === Sirio){
-      document.querySelector("div").style.backgroundColor = "#00FF00"
-      document.querySelector("div").style.transition="ease-in-out 0.8s"
+      document.querySelector("div").style.backgroundColor = "#FF00FF";
+      document.querySelector("div").style.transition = "ease-in-out 0.8s";
     } 
 
-    else{
-      document.querySelector("div").style.backgroundColor = "#FFFFFF"
+    else if(marca === Amazon){
+      document.querySelector("div").style.backgroundColor="#00FF00"
+    } 
+
+    else if(marca === Sirio){
+      document.querySelector("div").style.backgroundColor="tomato"
     }
+
   })
   
   return (
-    <Section>
-    <GlobalStyle />
-    <Informacoes>
-      <img src={marca} alt="vai na web logo" />
+    <Main>
+
+      <GlobalStyle />
+      <Informacao>
+      <img src={marca} alt="" />
       <h2>PATROCINADORA OFICIAL DO CICLO 'TALENTOS DIGITAIS DA ESCOLA VAI NA WEB         
       </h2>
-    </Informacoes>
+    </Informacao>
 
     <Patrocinadores>
         <img onClick={() => { setMarca(Amazon) }} src={Amazon} alt="" />
         <img onClick={() => { setMarca(Vnw) }} src={Vnw} alt="" />
         <img onClick={() => { setMarca(Sirio) }} src={Sirio} alt="" />
     </Patrocinadores>
-    </Section>
+    </Main>
   )
 }
 
